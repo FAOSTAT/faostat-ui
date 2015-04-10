7452f22a01d5d04550c10321b3d7d88207971f0c
Kalimaha <guido.barbaglia@gmail.com>
2015-04-10 11:49:48 +0200

    Task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,31 @@
 
 module.exports = function (grunt) {
 
-    /* Load JSON Schema utilities. */
-    var defaults = require('json-schema-defaults');
-
     /* Initiate Grunt configuration. */
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
     });
 
-    /* Load JSON Schema. */
-    var schema = grunt.file.readJSON('config/faostat_schema.json', [, {
-        encoding: 'utf8'
-    }]);
-
-    /* Create JSON. */
-    var json = defaults(schema);
-
-    /* Write JSON. */
-    grunt.file.write('config/faostat.json', JSON.stringify(json), [, {encoding: 'utf8'}]);
-
     /* Register default task. */
     grunt.registerTask('default', []);
+
+    /* Create configuration file. */
+    grunt.registerTask('create_configuration_file', 'Create JSON configuration file from JSON Schema.', function() {
+
+        /* Load JSON Schema utilities. */
+        var defaults = require('json-schema-defaults');
+
+        /* Load JSON Schema. */
+        var schema = grunt.file.readJSON('config/faostat_schema.json', [, {
+            encoding: 'utf8'
+        }]);
+
+        /* Create JSON. */
+        var json = defaults(schema);
+
+        /* Write JSON. */
+        grunt.file.write('config/faostat.json', JSON.stringify(json), [, {encoding: 'utf8'}]);
+
+    });
 
 };
