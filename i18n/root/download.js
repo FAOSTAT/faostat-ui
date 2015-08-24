cb72804a688bfda7572f1ebe4d65a705dfb37b5e
simonefenix <simone.murzilli@fao.org>
2015-08-24 17:17:23 +0200

    added submodule

---
+++ i18n/root/download.js
@@ -0,0 +1,8 @@
+    /*global define*/
+define({
+    "root": true,
+    "tree": "tree",
+    "metadata": "metadata",
+    "interactive_download": "interactive_download",
+    "bulk_download": "bulk_download"
+});
