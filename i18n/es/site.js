33fd5829d683883f96890a3d8562f58fa812e8a2
simonefenix <simone.murzilli@fao.org>
2015-10-06 18:44:04 +0200

    added multilanguage for territorial notes and feedback system

--- i18n/es/site.js
+++ i18n/es/site.js
@@ -1,7 +1,10 @@
 /*global define*/
 define({
 
-    fao: 'ORGANIZACIÓN DE LAS NACIONES UNIDAS PARA LA ALIMENTACIÓN Y LA AGRICULTURA',
-    statistics_division: 'Dirección  DE ESTADÍSTICA'
+    fao: 'Organización de las Naciones Unidas para la Alimentación y la Agricultura',
+    statistics_division: 'Dirección de Estadística',
+    feedback_system: 'AYÚDENOS CON SUS COMENTARIOS - VAYA AL SISTEMA DE COMENTARIOS',
+    territorial_notes: 'País / Territorio Notas',
+    territorial_notes_info: 'Existe una disputa de soberanía entre los Gobiernos de la Argentina y el Reino Unido de Gran Bretaña e Irlanda del Norte relativa a la soberanía de las Islas Malvinas (Falkland Islands).'
 
 });
