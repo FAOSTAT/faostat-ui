39eee635f9974f4580f704a0c22525c16bf0dba9
simonefenix <simone.murzilli@fao.org>
2015-10-06 17:24:34 +0200

    added i18n

--- i18n/es/site.js
+++ i18n/es/site.js
@@ -1,22 +1,7 @@
 /*global define*/
 define({
-    "footer_row_1": "Change me in i18n/root/site.js",
-    FAO_HEADER_ABOUT : 'About FAO',
-    FAO_HEADER_ABOUT_LINK : '',
-    FAO_HEADER_ACTION : 'In Action',
-    FAO_HEADER_ACTION_LINK : '',
-    FAO_HEADER_COUNTRIES : 'Countries',
-    FAO_HEADER_COUNTRIES_LINK : '',
-    FAO_HEADER_THEMES : 'Themes',
-    FAO_HEADER_THEMES_LINK : '',
-    FAO_HEADER_MEDIA : 'Media',
-    FAO_HEADER_MEDIA_LINK : '',
-    FAO_HEADER_PUBLICATIONS : 'Publications',
-    FAO_HEADER_PUBLICATIONS_LINK : '',
-    FAO_HEADER_STATISTICS : 'Statistics',
-    FAO_HEADER_STATISTICS_LINK : '',
-    FAO_HEADER_PARTNERSHIPS : 'Partnerships',
-    FAO_HEADER_PARTNERSHIPS_LINK : ''
 
+    fao: 'ORGANIZACIÓN DE LAS NACIONES UNIDAS PARA LA ALIMENTACIÓN Y LA AGRICULTURA',
+    statistics_division: 'Dirección  DE ESTADÍSTICA'
 
 });
