2044c71cb7b053bb55d3273abaa2f8ef4d2489c8
simonefenix <simone.murzilli@fao.org>
2015-11-03 14:15:42 +0100

    .

--- config/compare/Config.js
+++ config/compare/Config.js
@@ -1,75 +1,75 @@
 /*global define*/
 define(function () {
 
-        'use strict';
+    'use strict';
 
-        return {
+    return {
 
-            timerange: {
-               options: {
-                   bounds: {
-                       min: 1961,
-                       max: 2050
-                   },
-                   defaultValues:{
-                       min: 1990,
-                       max: 2013
-                   },
-                   step: 1
-               }
-            },
-
-            groups: {
-                blacklist: ['P']
-            },
-
-            domains: {
-                blacklist: ['QD']
-            },
-
-            // this will blacklist all the IDs that should not be created as filters
-            filters: {
-                blacklistCodesID: ['yeargroup']
-            },
-
-            series: {
-                blacklistCodesID: ['yeargroup']
-            },
-
-            // numeber of maximum series shown in the chart
-            seriesLimit: 20,
-
-            // chart definition
-            chart: {
-                model: null,
-                adapter: {
-                    adapterType: 'faostat',
-                    type: "timeserie",
-                    xDimensions: 'yeargroup',
-                    yDimensions: 'unit',
-                    valueDimensions: 'value',
-                    seriesDimensions: ['areagroup', 'elementgroup', 'itemgroup']
+        timerange: {
+            options: {
+                bounds: {
+                    min: 1961,
+                    max: 2050
                 },
-                template: {},
-                creator: {},
-                prepareChart: true
-            },
-
-            getData: {
-                limit:-1,
-                null_values:false,
-                thousand_separator:",",
-                decimal_separator:".",
-                decimal_places:2,
-                List1Codes: null,
-                List2Codes: null,
-                List3Codes: null,
-                List4Codes: null,
-                List5Codes: null,
-                List6Codes: null,
-                List7Codes: null,
-                output_type: 'arrays'
+                defaultValues:{
+                    min: 1990,
+                    max: 2014
+                },
+                step: 1
             }
+        },
 
-        };
+        groups: {
+            blacklist: ['P']
+        },
+
+        domains: {
+            blacklist: ['QD']
+        },
+
+        // this will blacklist all the IDs that should not be created as filters
+        filters: {
+            blacklistCodesID: ['yeargroup']
+        },
+
+        series: {
+            blacklistCodesID: ['yeargroup']
+        },
+
+        // numeber of maximum series shown in the chart
+        seriesLimit: 20,
+
+        // chart definition
+        chart: {
+            model: null,
+            adapter: {
+                adapterType: 'faostat',
+                type: "timeserie",
+                xDimensions: 'yeargroup',
+                yDimensions: 'unit',
+                valueDimensions: 'value',
+                seriesDimensions: ['areagroup', 'elementgroup', 'itemgroup']
+            },
+            template: {},
+            creator: {},
+            prepareChart: true
+        },
+
+        getData: {
+            limit:-1,
+            null_values:false,
+            thousand_separator:",",
+            decimal_separator:".",
+            decimal_places:2,
+            List1Codes: null,
+            List2Codes: null,
+            List3Codes: null,
+            List4Codes: null,
+            List5Codes: null,
+            List6Codes: null,
+            List7Codes: null,
+            output_type: 'arrays'
+        }
+
+    };
 });
