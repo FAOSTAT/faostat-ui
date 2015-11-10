57751621ce9e8508e0afe6b6aa27d1c2ae380977
simonefenix <simone.murzilli@fao.org>
2015-11-10 11:19:31 +0100

    filtr labels

---
+++ i18n/root/filter.js
@@ -0,0 +1,13 @@
+/*global define*/
+define(['jquery', 'i18n!nls/common'], function ($, Common) {
+
+    'use strict';
+
+    return $.extend(true, {}, Common, {
+
+        "browse_by_domain": "By Domain",
+        "browse_by_country": "By Country/Region",
+        "rankings": "Rankings"
+
+    });
+});
