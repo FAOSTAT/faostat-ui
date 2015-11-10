f6ce399badc757ad868ceedf1f9aca23e0f381fe
simonefenix <simone.murzilli@fao.org>
2015-11-10 17:50:21 +0100

    .

--- config/browse_by_domain/prod/views/QC.js
+++ config/browse_by_domain/prod/views/QC.js
@@ -77,78 +77,6 @@ define(function () {
                     ]
                 }
             },
-            /* {
-             "id": "year",
-             "type": "static",
-             "componentType": {
-             "class": "col-lg-2",
-             "type": "dropDownList"
-             },
-             "config": {
-             "data": [
-             {"code": "2012", "label": "2012", "selected": false},
-             {"code": "2011", "label": "2011", "selected": false},
-             {"code": "2010", "label": "2010", "selected": false},
-             {"code": "2009", "label": "2009", "selected": false},
-             {"code": "2008", "label": "2008", "selected": false},
-             {"code": "2007", "label": "2007", "selected": false},
-             {"code": "2006", "label": "2006", "selected": false},
-             {"code": "2005", "label": "2005", "selected": false},
-             {"code": "2004", "label": "2004", "selected": false},
-             {"code": "2003", "label": "2003", "selected": false},
-             {"code": "2002", "label": "2002", "selected": false},
-             {"code": "2001", "label": "2001", "selected": false},
-             {"code": "2000", "label": "2000", "selected": false},
-             {"code": "1999", "label": "1999", "selected": false},
-             {"code": "1998", "label": "1998", "selected": false},
-             {"code": "1997", "label": "1997", "selected": false},
-             {"code": "1996", "label": "1996", "selected": false},
-             {"code": "1995", "label": "1995", "selected": false},
-             {"code": "1994", "label": "1994", "selected": false},
-             {"code": "1993", "label": "1993", "selected": false},
-             {"code": "1992", "label": "1992", "selected": false},
-             {"code": "1991", "label": "1991", "selected": false},
-             {"code": "1990", "label": "1990", "selected": true}
-             ]
-             }
-             },
-             {
-             "id": "year",
-             "type": "static",
-             "componentType": {
-             <!-- TODO: add a class instead of bootstrap -->
-             "class": "col-lg-2",
-             "type": "dropDownList"
-             //"multiple": true
-             },
-             "config": {
-             "data": [
-             {"code": "2012", "label": "2012", "selected": true},
-             {"code": "2011", "label": "2011", "selected": false},
-             {"code": "2010", "label": "2010", "selected": false},
-             {"code": "2009", "label": "2009", "selected": false},
-             {"code": "2008", "label": "2008", "selected": false},
-             {"code": "2007", "label": "2007", "selected": false},
-             {"code": "2006", "label": "2006", "selected": false},
-             {"code": "2005", "label": "2005", "selected": false},
-             {"code": "2004", "label": "2004", "selected": false},
-             {"code": "2003", "label": "2003", "selected": false},
-             {"code": "2002", "label": "2002", "selected": false},
-             {"code": "2001", "label": "2001", "selected": false},
-             {"code": "2000", "label": "2000", "selected": false},
-             {"code": "1999", "label": "1999", "selected": false},
-             {"code": "1998", "label": "1998", "selected": false},
-             {"code": "1997", "label": "1997", "selected": false},
-             {"code": "1996", "label": "1996", "selected": false},
-             {"code": "1995", "label": "1995", "selected": false},
-             {"code": "1994", "label": "1994", "selected": false},
-             {"code": "1993", "label": "1993", "selected": false},
-             {"code": "1992", "label": "1992", "selected": false},
-             {"code": "1991", "label": "1991", "selected": false},
-             {"code": "1990", "label": "1990", "selected": false}
-             ]
-             }
-             },*/
             {
                 "id": "aggregation",
                 "type": "static",
@@ -167,21 +95,23 @@ define(function () {
             }
         ],
 
-        //"dashboard": {}
-
-
-
         dashboard: {
-            //data cube's uid
-            uid: "FAOSTAT_QC",
 
             //data base filter
-            filter: [],
+            filter: {
+                lang: 'en',
+                datasource: 'production',
+                decimal_places: 2,
+                decimal_separator: ".",
+                limit: -1,
+                thousand_separator: ",",
+                "null_values": null
+            },
 
             //bridge configuration
             bridge: {
 
-                type: "d3p"
+                type: "faostat"
 
             },
 
@@ -192,189 +122,32 @@ define(function () {
                     //id: 'faostat-QC-1',
                     type: 'map',
                     class: "col-lg-12",
-                    //height: "450px",
-                    //needed if layout = injected
-                    //container: "#faostat-QC-1",
+                    height:'250px',
                     config: {
-                        //container: "#faostat-QC-1",
                         leaflet: {
                             zoomControl: false,
                             attributionControl: true,
                             scrollWheelZoom: false,
-                            minZoom: 2
-                        }
-                    },
-                    // for now it takes the id, TODO: add uid as well
-                    allowedFilter: ['year', 'element', 'item'],
-                    filter: [
-                        {
-                            "name": "filter",
-                            "parameters": {
-                                "rows": {
-                                    "year": {
-                                        "time": [
-                                            {
-                                                "from": 2013,
-                                                "to": 2013
-                                            }
-                                        ]
-                                    },
-                                    "element": {
-                                        "codes": [
-                                            {
-                                                "uid": "FAOSTAT_Elements",
-                                                "codes": [
-                                                    "5312"
-                                                ]
-                                            }
-                                        ]
-                                    },
-                                    "item": {
-                                        "codes": [
-                                            {
-                                                "uid": "FAOSTAT_Items",
-                                                "codes": [
-                                                    "1717"
-                                                ]
-                                            }
-                                        ]
-                                    }
-                                }
-                            }
-                        }
-                    ]
-                },
-                {
-                    //id: 'faostat-QC-2',
-                    type: 'chart',
-                    class: "col-lg-6",
-                    height: "300px",
-                    //needed if layout = injected
-                    //container: "#faostat-QC-2",
-                    config: {
-                        //container: "#faostat-QC-2",
+                            minZoom: 1
+                        },
                         adapter: {
-                            type: "timeserie",
-                            xDimensions: 'time',
-                            yDimensions: 'element',
-                            valueDimensions: 'value',
-                            seriesDimensions: ["element"]
-                        },
-                        template: {
-                            //"title": "Top 25..."
-                        },
-                        creator: {
+                            adapterType: 'faostat',
+                            modelType: 'faostat'
                         }
                     },
                     // for now it takes the id, TODO: add uid as well
-                    allowedFilter: ['country', 'item', 'element'],
-                    filter: [
+                    allowedFilter: ['year', 'aggregation', 'item'],
+                    filter:
                         {
-                            "name": "filter",
-                            "parameters": {
-                                "rows": {
-
-                                    "country": {
-                                        "codes": [
-                                            {
-                                                "uid": "FAOSTAT_Countries",
-                                                "codes": [
-                                                    "33"
-                                                ]
-                                            }
-                                        ]
-                                    },
-                                    "element": {
-                                        "codes": [
-                                            {
-                                                "uid": "FAOSTAT_Elements",
-                                                "codes": [
-                                                    "5312"
-                                                ]
-                                            }
-                                        ]
-                                    },
-                                    "item": {
-                                        "codes": [
-                                            {
-                                                "uid": "FAOSTAT_Items",
-                                                "codes": [
-                                                    "1717"
-                                                ]
-                                            }
-                                        ]
-
-                                    }
-                                }
-                            }
+                            domain_code: 'QC',
+                            List1Codes: ["5000>"],
+                            List2Codes: ["2510"],
+                            List3Codes: ["515"],
+                            List4Codes: ["2011"],
+                            List5Codes: null,
+                            List6Codes: null,
+                            List7Codes: null
                         }
-                    ]
-                },
-                {
-                    //id: 'faostat-QC-2',
-                    type: 'chart',
-                    class: "col-lg-6",
-                    height: "450px",
-                    //needed if layout = injected
-                    //container: "#faostat-QC-2",
-                    config: {
-                        //container: "#faostat-QC-2",
-                        adapter: {
-                            type: "timeserie",
-                            xDimensions: 'time',
-                            yDimensions: 'element',
-                            valueDimensions: 'value',
-                            seriesDimensions: ["element"]
-                        },
-                        template: {
-                            //"title": "Top 25..."
-                        },
-                        creator: {
-                        }
-                    },
-                    // for now it takes the id, TODO: add uid as well
-                    allowedFilter: ['country', 'item', 'element'],
-                    filter: [
-                        {
-                            "name": "filter",
-                            "parameters": {
-                                "rows": {
-
-                                    "country": {
-                                        "codes": [
-                                            {
-                                                "uid": "FAOSTAT_Countries",
-                                                "codes": [
-                                                    "33"
-                                                ]
-                                            }
-                                        ]
-                                    },
-                                    "element": {
-                                        "codes": [
-                                            {
-                                                "uid": "FAOSTAT_Elements",
-                                                "codes": [
-                                                    "5312"
-                                                ]
-                                            }
-                                        ]
-                                    },
-                                    "item": {
-                                        "codes": [
-                                            {
-                                                "uid": "FAOSTAT_Items",
-                                                "codes": [
-                                                    "1717"
-                                                ]
-                                            }
-                                        ]
-
-                                    }
-                                }
-                            }
-                        }
-                    ]
                 }
 
             ]
