5b4bc9d52be0063ea7683e6b59ca6da17acb8c9f
simonefenix <simone.murzilli@fao.org>
2015-11-16 16:59:06 +0100

    .

--- i18n/fr/browse.js
+++ i18n/fr/browse.js
@@ -8,7 +8,9 @@ define(['jquery', 'i18n!nls/common'], function ($, Common) {
         "tree_title": "FAOSTAT Domains",
         "browse_by_domain": "By Domain",
         "browse_by_country": "By Country/Region",
-        "rankings": "Rankings"
+        "rankings": "Rankings",
+
+        "map_footer": "Les appellations employées et la présentation des données sur la carte n'impliquent de la part de la FAO aucune prise de position quant au statut juridique ou constitutionnel des pays, territoires ou zones maritimes, ni quant au tracé de leurs frontières ou limites. Le Soudan du Sud a déclaré son indépendance le 9 Juillet 2011. Pour des raisons de disponibilité des données, l’évaluation montrée sur la carte à la fois pour le Soudan et le Soudan du Sud, refléte la situation comme elle l’etait jusqu’en 2011 pour l’ex Soudan."
 
     });
 });
