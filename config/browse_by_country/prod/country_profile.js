ee124ed92fa453900ff43acb5c6bfdfd9724b107
simonefenix <simone.murzilli@fao.org>
2015-11-20 17:20:13 +0100

    country profile view

---
+++ config/browse_by_country/prod/country_profile.js
@@ -0,0 +1,300 @@
+/*global define*/
+
+define(function () {
+
+    'use strict';
+
+    return {
+
+        "filter": {
+
+            defaultFilter: {
+                "domain_code": ["QC"]
+            },
+
+            items: [
+                {
+                    "id": "item",
+                    "type": "codelist",
+                    // TODO: in theory that should come from the dimensions schema!!
+                    "parameter": "List3Codes",
+                    //"title": "title",
+                    "componentType": {
+                        "class": "col-lg-3",
+                        "type": "dropDownList"
+                    },
+                    "config": {
+                        "dimension_id": "items",
+                        "defaultCodes": ["27"],
+                        "filter": {
+                        }
+                    }
+                },
+                {
+                    // id to be applied on the getData request
+                    "id": "area",
+                    "type": "codelist",
+                    "parameter": "List1Codes",
+                    "componentType": {
+                        <!-- TODO: add a class instead of bootstrap -->
+                        "class": "col-lg-3",
+                        "type": "dropDownList"
+                        //"multiple": true
+                    },
+                    "config": {
+                        "dimension_id": "countries",
+                        "defaultCodes": ["2"],
+                        "filter": {
+                        }
+                    }
+                },
+                {
+                    "id": "year",
+                    "type": "codelist",
+                    "parameter": "List4Codes",
+                    "componentType": {
+                        "class": "col-lg-2",
+                        "type": "dropDownList-timerange"
+                    },
+                    "config": {
+                        "dimension_id": "year",
+                        "defaultCodes": ['1993'],
+                        "filter": {
+                        }
+                    }
+                },
+                {
+                    "id": "aggregation",
+                    "type": "static",
+                    // TODO: check data parameter
+                    "parameter": "operator",
+                    "componentType": {
+                        "class": "col-lg-2",
+                        "type": "dropDownList"
+                    },
+                    "config": {
+                        "defaultCodes": ["AVG"],
+                        "data": [
+                            // TODO: multilingual?
+                            {"code": "AVG", "label": "average", "selected": true},
+                            {"code": "SUM", "label": "sum", "selected": false}
+                        ]
+                    }
+                }
+            ]
+        },
+
+        dashboard: {
+
+            //data base filter
+            defaultFilter: {
+                domain_code: 'QC',
+                List2Codes: ["2510"],
+                List5Codes: null,
+                List6Codes: null,
+                List7Codes: null,
+                decimal_places: 2,
+                decimal_separator: ".",
+                limit: -1,
+                thousand_separator: ",",
+                "null_values": null
+            },
+
+            // labels?
+            labels: {
+                // labels to dinamically substitute the title and subtitle
+                default: {
+                    /*aggregation: {
+                     en: "Average",
+                     fr: "Moyenne",
+                     es: "Promedio"
+                     },
+                     year: "1993 - 2013",
+                     area: {
+                     "en": "Afghanistan",
+                     "fr": "Afghanistan",
+                     "es": "Afghanistan"
+                     },
+                     item: {
+                     "en": "Rice, milled",
+                     "fr": "Rice, milled",
+                     "es": "Rice, milled"
+                     }*/
+                }
+            },
+
+
+            //bridge configuration
+            bridge: {
+
+                type: "faostat",
+                requestType: 'data' // data, rankings
+
+            },
+
+            metadata: {},
+
+            items: [
+                {
+                    type: 'map',
+                    class: "col-xs-12",
+
+                    // labels?
+                    labels: {
+                        // labels to dinamically substitute the title and subtitle
+                        default: {},
+
+                        // temp[late to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Production quantities of {{item}} by country",
+                                fr: "Production quantities of {{item}} by country",
+                                es: "Production quantities of {{item}} by country"
+                            },
+                            subtitle: "{{aggregation}} {{year}}"
+                        }
+                    },
+
+
+                    //height:'250px',
+                    config: {
+                        template: {}
+                    },
+                    allowedFilter: ['item', 'year', 'element', 'aggregation'],
+                    deniedTemplateFilter: [],
+                    filter: {
+                        // TODO: remove the List1Codes (in theory should be automatically detected from the domain dimensions/schema)
+                        List1Codes: ["5000>", "351"],
+                        "group_by": 'year',
+                        "order_by": 'area'
+                    },
+/*                    bridge: {
+                        requestType: 'rankings' // data, rankings
+                    }*/
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels?
+                    labels: {
+                        // temp[template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Production quantities of {{item}} in {{area}}",
+                                fr: "Production de {{item}} dans le {{area}}",
+                                es: "Producción de {{item}} en {{area}}"
+                            },
+                            subtitle: "{{year}}"
+                        }
+                    },
+                    
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "timeserie",
+                            xDimensions: 'year',
+                            yDimensions: 'unit',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['area', 'item', 'element']
+                        },
+                        template: {
+                            // height:'350px'
+                            // default labels to be applied
+                        },
+                        creator: {}
+                    },
+                    allowedFilter: ['area', 'year', 'item'],
+                    filter: {}
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels?
+                    labels: {
+                        // temp[late to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Production share of {{item}} by region",
+                                fr: "Part de la production de {{item}} par région",
+                                es: "Proporción de producción de {{item}} por región"
+                            },
+                            subtitle: "{{aggregation}} {{year}}"
+                        }
+
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "pie",
+                            xDimensions: null,
+                            yDimensions: null,
+                            valueDimensions: 'value',
+                            seriesDimensions: ['area']
+                        },
+                        template: {
+                            height: '250px'
+                        },
+                        creator: {}
+                    },
+                    allowedFilter: ['year', 'item', 'aggregation'],
+                    filter: {
+                        // TODO: remove the List1Codes (in theory should be automatically detected from the domain dimensions/schema)
+                        List1Codes: ["5100", "5200", "5300", "5400", "5500"],
+                        "group_by": 'year',
+                        "order_by": 'area'
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels?
+                    labels: {
+                        // temp[template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Production of {{item}} top 5 producers",
+                                fr: "Production of {{item}} top 5 producers",
+                                es: "Production of {{item}} top 5 producers"
+                            },
+                            subtitle: "{{year}}"
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "standard",
+                            xDimensions: ['element'],
+                            yDimensions: 'unit',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['area']
+                        },
+                        template: {
+                            // height:'350px'
+                            // default labels to be applied
+                        },
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+                    allowedFilter: ['year', 'item', 'aggregation'],
+                    filter: {
+                        List1Codes: ["5000>"],
+                        "group_by": 'year',
+                        "order_by": 'value DESC',
+                        "limit": '10'
+                    }
+                }
+            ]
+        }
+
+    }
+});
