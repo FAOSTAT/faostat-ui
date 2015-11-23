f25bde542a684f9cd15ac3c99072bb9df08d9431
simonemurzilli <simone.murzilli@fao.org>
2015-11-23 10:39:42 +0100

    .

--- i18n/root/browse_rankings.js
+++ i18n/root/browse_rankings.js
@@ -5,7 +5,8 @@ define(['jquery', 'i18n!nls/browse'], function ($, I18N) {
 
     return $.extend(true, {}, I18N, {
 
-        "tree_title": "Rankings"
+        "tree_title": "Rankings",
+        "select_a_node": "Please select one of the domains"
 
     });
 
