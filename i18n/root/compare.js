5165bcda08bd7bc34033c1818772afea9f868a78
geoaddiction@gmail.com <simone.murzilli@fao.org>
2015-11-24 15:04:03 +0100

    .

--- i18n/root/compare.js
+++ i18n/root/compare.js
@@ -11,7 +11,8 @@ define(['jquery', 'i18n!nls/common'], function ($, Common) {
         "add_new_selector": "Add New Selector",
         "compare_data": "Compare Data",
         "add_filter": "Add Filter",
-        "remove_filter": "Remove Filter"
+        "remove_filter": "Remove Filter",
+        "chart_title": "Timeseries on selected data"
 
     });
 });
