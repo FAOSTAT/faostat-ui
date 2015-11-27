2fc58322b6fd6c6c4d9bbe2333d662fa0a631430
simonemurzilli <simone.murzilli@fao.org>
2015-11-27 13:19:08 +0100

    updated what's new and coming up sections

--- config/home/sample/comingUp.js
+++ config/home/sample/comingUp.js
@@ -4,19 +4,21 @@ define([], function () {
     'use strict';
 
     return {
+
         data: [
             {
-                title: "NOVEMBER 2015",
-                text: "Research and Development indicators, ASTI."
-            },
-            {
-                title: "NOVEMBER 2015",
-                text: "Food Aid Shipments (WFP)."
+                title: "OCTOBER 2015",
+                text: "Investment."
             },
             {
                 title: "OCTOBER 2015",
-                text: '<a href="http://www.fao.org/economic/ess/ess-events/wsd/en/" target="_blank">World Statistics Day</a>'
+                text: "Fertilizers detailed and Trade value."
+            },
+            {
+                title: "OCTOBER 2015",
+                text: 'Population – Employment Indicators.'
             }
         ]
+
     }
 });
--- config/home/sample/whatsNew.js
+++ config/home/sample/whatsNew.js
@@ -4,6 +4,7 @@ define([], function () {
     'use strict';
 
     return {
+
         data: [
             {
                 title: "NOVEMBER 2015",
@@ -16,7 +17,28 @@ define([], function () {
             {
                 title: "OCTOBER 2015",
                 text: '<a href="http://www.fao.org/economic/ess/ess-events/wsd/en/" target="_blank">World Statistics Day</a>'
+            },
+            {
+                title: "OCTOBER 2015",
+                text: '<a href="http://www.fao.org/world-food-day/home/en/" target="_blank">World Food Day</a>'
+            },
+            {
+                title: "OCTOBER 2015",
+                text: "Population."
+            },
+            {
+                title: "SEPTEMBER 2015",
+                text: '2013 updates for Annual and Monthly producer prices, Annual producer price indices and value of Agricultural production.'
+            },
+            {
+                title: "SEPTEMBER 2015",
+                text: '2013 update for Land.'
+            },
+            {
+                title: "SEPTEMBER 2015",
+                text: 'New dataset on Deflators to 2013.'
             }
         ]
+
     }
 });
