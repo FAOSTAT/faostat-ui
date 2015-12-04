6d12d5b57710ad084a0227fe759c3cfc1e8975e3
simonemurzilli <simone.murzilli@fao.org>
2015-12-04 14:03:11 +0100

    added label

--- i18n/root/browse_by_country.js
+++ i18n/root/browse_by_country.js
@@ -7,7 +7,8 @@ define(['jquery', 'i18n!nls/browse'], function ($, I18N) {
 
         filter_results: 'Search Country',
         sort_by_country: 'Sort by Country name',
-        back_to_country_list: "Back to countries' list"
+        back_to_country_list: "Back to countries' list",
+        country_list: "Country List"
 
     });
 
