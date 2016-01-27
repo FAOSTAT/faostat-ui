7f6a4357572b047a91831a78455866be4f4c8155
simonemurzilli <simone.murzilli@fao.org>
2016-01-27 22:32:47 +0100

    added text option to waiting

--- src/js/lib/common/waiting.js
+++ src/js/lib/common/waiting.js
@@ -14,7 +14,7 @@ define([
 
     var waiting = $('<div class="modal" data-backdrop="static" data-keyboard="false" >' +
         '<span style="color: #eeeeee;opacity: 0.9; position: absolute;top: 50%;left: 50%;">' +
-        '<i class="fa fa-cog fa-spin fa-4x"></i>' +
+        '<i class="fa fa-cog fa-spin fa-4x"></i><h5 data-role="text"></h5>' +
         '</span>' +
         '</div>');
 
@@ -26,12 +26,17 @@ define([
 
     Waiting.prototype.showPleaseWait = function (data) {
 
+        if (data && data.hasOwnProperty('text')) {
+            waiting.find("[data-role='text']").html(data.text);
+        }
+
         waiting.modal('show');
 
     };
 
     Waiting.prototype.hidePleaseWait = function () {
 
+       waiting.find("[data-role='text']").html();
        waiting.modal('hide');
 
     };
