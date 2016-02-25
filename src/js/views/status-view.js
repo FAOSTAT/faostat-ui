47a7ba79883a48ccf3a3bd52066a07bb9fb9bcb5
simonemurzilli <simone.murzilli@fao.org>
2016-02-25 18:29:28 +0100

    added status

--- src/js/views/status-view.js
+++ src/js/views/status-view.js
@@ -8,12 +8,13 @@ define([
     'views/base/view',
     'config/Config',
     'config/Events',
-    'text!templates/indicators/indicators.hbs',
+    'text!templates/status/status.hbs',
+    'text!templates/status/status_api.hbs',
     'i18n!nls/indicators',
     'fs-r-t/start',
     'faostatapiclient',
-    'text!templates/indicators/testing.hbs',
     'q',
+    'handlebars',
     'amplify'
 ], function ($,
              log,
@@ -22,11 +23,12 @@ define([
              C,
              E,
              template,
+             templateAPI,
              i18nLabels,
              ReportTable,
              FAOSTATApi,
-             templateTesting,
-             Q
+             Q,
+             Handlebars
              ) {
 
     'use strict';
@@ -35,6 +37,14 @@ define([
         IndicatorsView;
 
     s = {
+
+        GROUPS: '#fs-status-groups',
+        DOMAINS: '#fs-status-domains',
+        DOMAINS_DETAILS: '#fs-status-domains-details',
+        WARNING: '#fs-status-warning',
+        DIMENSIONS: '#fs-status-dimensions',
+        CODES: '#fs-status-codes',
+        DATA: '#fs-status-data'
 
     };
 
@@ -59,7 +69,7 @@ define([
             View.prototype.attach.call(this, arguments);
 
             /* Update State. */
-            amplify.publish(E.STATE_CHANGE, {indicators: 'indicators'});
+            amplify.publish(E.STATE_CHANGE, {status: 'status'});
 
             this.initVariables();
 
@@ -73,87 +83,120 @@ define([
 
         initVariables: function () {
 
-            var self = this;
-
             this.api = new FAOSTATApi();
 
-            /*api.data({
-                domain_codes: ['QC'],
-                filters: {
-                    area: ['5000>'],
-                    elements: ['2510'],
-                    items: ['15'],
-                    //years: ['2012']
-                },
+            this.$GROUPS = this.$el.find(s.GROUPS);
+            this.$DOMAINS = this.$el.find(s.DOMAINS);
+            this.$DOMAINS_DETAILS = this.$el.find(s.DOMAINS_DETAILS);
+            this.$WARNING = this.$el.find(s.WARNING);
+            this.$DIMENSIONS = this.$el.find(s.DIMENSIONS);
+            this.$CODES = this.$el.find(s.CODES);
+            this.$DATA = this.$el.find(s.DATA);
 
-                //output_type: 'csv'
-            }).then(function (response) {
-                log.info(response)
-            });*/
+            amplify.publish(E.LOADING_SHOW, {container: this.$GROUPS});
+            amplify.publish(E.LOADING_SHOW, {container: this.$DOMAINS});
+            //amplify.publish(E.LOADING_SHOW, {container: this.$DOMAINS_DETAILS});
+            //amplify.publish(E.LOADING_SHOW, {container: this.$DIMENSIONS});
+            //amplify.publish(E.LOADING_SHOW, {container: this.$CODES});
+            //amplify.publish(E.LOADING_SHOW, {container: this.$DATA});
 
-/*            api.databean({
-                domain_codes: ['QC'],
-                List1Codes: ['2'],
-                List2Codes: ['2510'],
-                List3Codes: ['15'],
-                List4Codes: ['2010'],
-                List5Codes: null,
-                List6Codes: null,
-                List7Codes: null,
-                List1AltCodes: ['ISO3'],
-                page_size: 10
-                //output_type: 'csv'
-            }).then(function (response) {
-                log.info(response);
-            });*/
+        },
 
+        initComponents: function () {
 
-            /*var reportTable = new ReportTable();
+            var self = this;
 
-            reportTable.init({
-                container: this.$el,
-                request: {
-                    domain_code: 'fbs',
-                    List1Codes: [33],
-                    List2Codes: [2009]
-                }
+            this.api.groups({
+                lang: this.o.lang,
+                datasource:  C.DATASOURCE
+            }).then(function(groups) {
+
+                self.showGroups(groups);
+
+            });
+
+           this.api.domainstree({
+                lang: this.o.lang,
+                datasource:  C.DATASOURCE
+            }).then(function(domains) {
+
+               self.showDomains(domains);
+
+                _.each(domains.data, function(domain, index) {
+
+                    self.getDataDomainSample(domain);
+
+                });
+
             });
 
 
-            reportTable.render();
-            //reportTable.export();*/
+        },
 
+        configurePage: function () {
 
+        },
 
-            this.api.domainstree({
-                lang: this.o.lang,
-                datasource:  C.DATASOURCE,
-            }).then(function(domains) {
+        showGroups: function (d) {
 
-                _.each(domains.data, function(domain) {
+            var self = this;
 
-                    self.getDataDomainSample(null, domain.DomainCode, domain.DomainName);
-                });
+            var html = $(templateAPI).filter('#groups').html(),
+                t = Handlebars.compile(html);
+
+            this.$GROUPS.html(t({
+                total: d.data.length,
+                data: d.data
+            }));
+
+            this.$GROUPS.find('[data-role="show-details"]').on('click', function(e) {
+                e.preventDefault();
+                self.$GROUPS.find('[data-role="details"]').toggleClass('hidden');
             });
 
         },
 
-        getDataDomainSample: function ($container, domainCode, domainName) {
+        showDomains: function (d) {
+
+            var self = this;
+
+            var html = $(templateAPI).filter('#domains').html(),
+                t = Handlebars.compile(html);
+
+            this.$DOMAINS.html(t({
+                total: d.data.length,
+                data: d.data
+            }));
+
+            this.$DOMAINS.find('[data-role="show-details"]').on('click', function(e) {
+                e.preventDefault();
+                self.$DOMAINS.find('[data-role="details"]').toggleClass('hidden');
+            });
+
+        },
+
+        getDataDomainSample: function (domain) {
 
             var self = this,
                 lang = this.o.lang,
                 datasource = C.DATASOURCE,
-                domain_code = domainCode;
+                domainCode = domain.DomainCode,
+                domainName = domain.DomainName,
+                html = $(templateAPI).filter('#domain-details').html(),
+                t = Handlebars.compile(html);
 
+            this.$DOMAINS_DETAILS.append(t(domain));
 
-            //$container.empty();
+            var $domain_container = this.$DOMAINS_DETAILS.find('[data-role="'+ domainCode +'"]');
 
             // get domain dimension
             this.api.dimensions({
                 lang: lang,
                 datasource: datasource,
-                domain_code: domain_code
+                domain_code: domainCode
             }).then(function(dimensions) {
+
+                self.showDetails($domain_container, 'dimensions', dimensions);
 
                 // TODO: show dimensions on $container
                 //log.info(dimensions);
@@ -167,7 +210,7 @@ define([
                     requestCodes.push(self.api.codes({
                         lang: lang,
                         datasource: datasource,
-                        domain_code: domain_code,
+                        domain_code: domainCode,
                         id: dimension.id,
                         subcodelists: false,
                         ord: true
@@ -184,6 +227,8 @@ define([
 
                     // for each code get a sample
                     _.each(codes, function(code) {
+
+                        self.showDetails($domain_container, 'codes', code);
 
                         //log.info(code);
 
@@ -203,13 +248,15 @@ define([
                             {
                                 lang: lang,
                                 datasource: datasource,
-                                domain_codes: [domain_code]
+                                domain_codes: [domainCode]
                             },
                             { filters: filters}
                         )).then(function (data) {
 
-                            log.info(domainName, domain_code, data.data.length);
+                            log.info(domainName, domainCode, data.data.length);
                             //log.info(data);
+
+                            self.showData($domain_container, data, domain);
 
                             data = null;
 
@@ -224,11 +271,49 @@ define([
 
         },
 
-        initComponents: function () {
+        showDetails: function ($container, type, d) {
+
+            var $details = $container.find('[data-role="'+ type + '-details"]');
+
+            var html = $(templateAPI).filter('#' + type).html(),
+                t = Handlebars.compile(html);
+
+            $details.append(t({
+                total: d.data.length,
+                data: d.data,
+                metadata: d.metadata
+            }));
 
         },
 
-        configurePage: function () {
+        showData: function ($container, d, domain) {
+
+            var $details = $container.find('[data-role="data-details"]');
+
+            var html = $(templateAPI).filter('#data').html(),
+                t = Handlebars.compile(html);
+
+            $details.append(t({
+                rows: d.data.length,
+                color: (d.data.length <= 0)? 'red': null,
+                status: (d.data.length <= 0)? 'Warning': null
+            }));
+
+
+            this.showWarning(d, domain);
+        },
+
+        showWarning: function(d, domain) {
+
+            if ( d.data.length <= 0 ) {
+                var html = $(templateAPI).filter('#warning').html(),
+                    t = Handlebars.compile(html);
+
+                this.$WARNING.append(t($.extend(true, {}, domain, {
+                    rows: d.data.length,
+                    color: (d.data.length <= 0)? 'red': null
+                })));
+            }
 
         },
 
