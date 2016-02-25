6369a3b4c5d5723dc8da3484bf71bb6472ee8ea4
simonemurzilli <simone.murzilli@fao.org>
2016-02-25 12:18:29 +0100

    added faq

--- src/js/controllers/faq-controller.js
+++ src/js/controllers/faq-controller.js
@@ -1,44 +1,21 @@
 /*global define*/
 define([
     'controllers/base/controller',
-    'views/standards-view'], function (Controller, View) {
+    'views/faq-view'], function (Controller, View) {
 
     'use strict';
 
-    var StandardsController = Controller.extend({
+    var FAQController = Controller.extend({
 
-        show: function (params, section) {
+        show: function (params) {
+
             this.view = new View({
-                region: 'main',
-                section: section,
-                lang: params.lang,
-                id: params.id !== undefined ? params.id.toUpperCase() : undefined
+                region: 'main'
             });
-        },
-
-        show_methodologies: function (params) {
-             //TODO: can be switched to the "name" section from ChaplinJS routes.js definition?
-            this.show(params, 'methodologies');
-        },
-
-        show_units: function (params) {
-            this.show(params, 'units');
-        },
-
-        show_abbreviations: function (params) {
-            this.show(params, 'abbreviations');
-        },
-
-        show_glossary: function (params) {
-            this.show(params, 'glossary');
-        },
-
-        show_classifications: function (params) {
-            this.show(params, 'classifications');
         }
 
     });
 
-    return StandardsController;
+    return FAQController;
 
 });
