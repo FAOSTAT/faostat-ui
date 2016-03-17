358a8ea6cfe937866fa8e95821ee805e203a402c
simonemurzilli <simone.murzilli@fao.org>
2016-03-17 14:56:06 +0100

    .

---
+++ config/browse_by_domain/prod/views/GB-PROJ.js
@@ -0,0 +1,330 @@
+/*global define*/
+define([
+    'config/browse_by_domain/Config'
+],function (C) {
+
+    'use strict';
+
+    var i18n = C.i18n || {};
+
+    return {
+
+        "relatedViews" : [
+            {
+                title: i18n.tab_ghg_main,
+                id: 'GB',
+                selected: true
+            },
+            {
+                title: i18n.projections,
+                id: 'GB-PROJ'
+            }
+        ],
+
+        "comment": {
+            "text": {
+                "en": "Emissions of methane and nitrous oxide from the on-site combustion of crop residues",
+                "fr": "Émissions de méthane et d'oxyde nitreux provenant de la combustion sur place des résidus de cultures",
+                "es": "Émissions de méthane et d'oxyde nitreux provenant de la combustion sur place des résidus de cultures"
+            }
+            //,pdf: "GT.pdf"
+        },
+
+        "filter": {
+
+            defaultFilter: {
+                "domain_code": ["GB"]
+            },
+
+            items: [
+                {
+                    "id": "item",
+                    "type": "codelist",
+                    // TODO: in theory that should come from the dimensions schema!!
+                    "parameter": "List3Codes",
+                    "componentType": {
+                        "class": "col-lg-3",
+                        "type": "dropDownList"
+                    },
+                    "config": {
+                        "dimension_id": "item",
+                        "defaultCodes": ["1755"],
+                        "filter": {
+                        }
+                    }
+                },
+                {
+                    // id to be applied on the getData request
+                    "id": "area",
+                    "type": "codelist",
+                    "parameter": "List1Codes",
+                    "componentType": {
+                        "class": "col-lg-3",
+                        "type": "dropDownList"
+                        //"multiple": true
+                    },
+                    "config": {
+                        "dimension_id": "area",
+                        "defaultCodes": ["5000"],
+                        "filter": {
+                        }
+                    }
+                },
+                {
+                    "id": "year",
+                    "type": "codelist",
+                    "parameter": "List4Codes",
+                    "componentType": {
+                        "class": "col-lg-2",
+                        "type": "dropDownList"
+                    },
+                    "config": {
+                        "dimension_id": "yearprojections",
+                        "defaultCodes": ['2030'],
+                        "filter": {
+                        }
+                    }
+                }
+            ]
+        },
+
+        dashboard: {
+
+            //data base filter
+            defaultFilter: {
+                domain_codes: ['GB'],
+                List2Codes: ["7231"],
+                List5Codes: null,
+                List6Codes: null,
+                List7Codes: null,
+                limit: -1,
+                decimal_places: 2,
+                thousand_separator: ",",
+                "null_values": null,
+                page_size: 0,
+                page_number: 0
+            },
+
+            // labels?
+            labels: {
+                // labels to dinamically substitute the title and subtitle
+                default: {
+                }
+            },
+
+
+            //bridge configuration
+            bridge: {
+
+                type: "faostat"
+                //requestType: 'data' // data, rankings
+
+            },
+
+            metadata: {},
+
+            items: [
+                {
+                    type: 'map',
+                    class: "col-xs-12",
+
+                    // labels?
+                    labels: {
+                        // labels to dinamically substitute the title and subtitle
+                        default: {},
+
+                        // temp[late to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Emissions by country (CO2 equivalent), {{item}}",
+                                fr: "Émissions par pays (CO2 équivalent), {{item}}",
+                                es: "Emisiones por país (CO2 equivalente), {{item}}"
+                            },
+                            subtitle: "{{aggregation}} {{year}}"
+                        }
+                    },
+
+
+                    //height:'250px',
+                    config: {
+                        layer: {
+                            colorramp: "YlOrRd",
+                            intervals: 7
+                        },
+                        template: {
+
+                        }
+                    },
+                    allowedFilter: ['item', 'year', 'element'],
+                    deniedTemplateFilter: [],
+                    filter: {
+                        // TODO: remove the List1Codes (in theory should be automatically detected from the domain dimensions/schema)
+                        List1Codes: ["5000>", "351"]
+                        //"group_by": 'year',
+                        //"order_by": 'area'
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-md-6",
+
+                    // labels?
+                    labels: {
+                        // template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Emissions (CO2 equivalent), {{item}}",
+                                fr: "Émissions (CO2 équivalent), {{item}}",
+                                es: "Emisiones (CO2 equivalente), {{item}}"
+                            },
+                            subtitle: ""
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "standard",
+                            xDimensions: 'area',
+                            yDimensions: 'unit',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['year']
+                        },
+                        template: {},
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+                    allowedFilter: ['item'],
+                    filter: {
+                        List1Codes: ["5100", "5200", "5300", "5400", "5500"],
+                        List4Codes: ["2030", "2050"]
+                        // TODO: baseline 2005-2006-2007
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-md-6",
+
+                    // labels?
+                    labels: {
+                        // template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Emissions (CO2 equivalent), {{item}}, {{area}}",
+                                fr: "Émissions (CO2 équivalent), {{item}}, {{area}}",
+                                es: "Emisiones (CO2 equivalente), {{item}}, {{area}}"
+                            },
+                            subtitle: ""
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "standard",
+                            xDimensions: 'area',
+                            yDimensions: 'unit',
+                            valueDimensions: 'value',
+                            seriesDimensions: ['year']
+                        },
+                        template: {},
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+                    allowedFilter: ['area', 'item'],
+                    filter: {
+                        List4Codes: ["2030", "2050"]
+                        // TODO: baseline 2005-2006-2007
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    // labels?
+                    labels: {
+                        // template to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "Emissions by continent (CO2 equivalent), {{item}}",
+                                fr: "Émissions par continent (CO2 équivalent), {{item}}",
+                                es: "Emisiones por continente (CO2 equivalente), {{item}}"
+                            },
+                            subtitle: "{{year}}"
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "pie",
+                            xDimensions: null,
+                            yDimensions: null,
+                            valueDimensions: 'value',
+                            seriesDimensions: ['area']
+                        },
+                        template: {},
+                        creator: {
+                            chartObj: {
+                                chart: {
+                                    type: "column"
+                                }
+                            }
+                        }
+                    },
+                    allowedFilter: ['year', 'item'],
+                    deniedOnLoadFilter: [],
+                    filter: {
+                        List1Codes: ["5100", "5200", "5300", "5400", "5500"]
+                    }
+                },
+                {
+                    type: 'chart',
+                    class: "col-xs-12",
+
+                    labels: {
+                        template: {
+                            title: {
+                                en: "Emissions by crop type (CO2 equivalent), {{area}}",
+                                fr: "Émissions par type de culture (CO2 équivalent), {{area}}",
+                                es: "Emissions  por tipo de cultivo (CO2 equivalente), {{area}}"
+                            },
+                            subtitle: "{{aggregation}} {{year}}"
+                        }
+                    },
+
+                    config: {
+                        adapter: {
+                            adapterType: 'faostat',
+                            type: "pie",
+                            xDimensions: null,
+                            yDimensions: null,
+                            valueDimensions: 'value',
+                            seriesDimensions: ['item']
+                        },
+                        template: {
+                            height: '250px'
+                        },
+                        creator: {
+                        }
+                    },
+                    allowedFilter: ['area', 'year', 'aggregation'],
+                    filter: {
+                        List3Codes: ["1712>"],
+                    }
+                }
+            ]
+        }
+
+    };
+});
