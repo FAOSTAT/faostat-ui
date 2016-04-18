4030ec6a175d8c11b3cb2cfdc4304372cec9f337
simonemurzilli <simone.murzilli@fao.org>
2016-04-18 16:58:08 +0200

    .

--- i18n/es/browse.js
+++ i18n/es/browse.js
@@ -9,7 +9,7 @@ define(['jquery', 'i18n!nls/common'], function ($, Common) {
         "browse_by_country": "Por País",
         "by_rankings": "Por Clasificaciones",
 
-        map_footer: "<i>The designations employed and the presentation of material in the maps do not imply the expression of any opinion whatsoever on the part of FAO concerning the legal or constitutional status of any country, territory or sea area, or concerning the delimitation of frontiers.                    South Sudan declared its independence on July 9, 2011. Due to data availability, the assessment presented in the map for Sudan and South Sudan reflects the situation up to 2011 for the former Sudan.</i>",
+        map_footer: "<i>Les appellations employées et la présentation des données sur la carte n'impliquent de la part de la FAO aucune prise de position quant au statut juridique ou constitutionnel des pays, territoires ou zones maritimes, ni quant au tracé de leurs frontières ou limites. Le Soudan du Sud a déclaré son indépendance le 9 Juillet 2011. Pour des raisons de disponibilité des données, l’évaluation montrée sur la carte à la fois pour le Soudan et le Soudan du Sud, refléte la situation comme elle l’etait jusqu’en 2011 pour l’ex Soudan.</i>",
         
     });
 });
