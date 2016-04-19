bbf7942e08767ccaa56689fa403ddc768a4ccd7c
simonemurzilli <simone.murzilli@fao.org>
2016-04-19 13:53:10 +0200

    i18n abbreviations

--- i18n/es/standards-abbreviations.js
+++ i18n/es/standards-abbreviations.js
@@ -4,11 +4,9 @@ define(['jquery', 'i18n!nls/standards'], function ($, Standards) {
     'use strict';
 
     return $.extend(true, {}, Standards, {
-
-        title: 'Abbreviations List',
-        code_title: 'Acronym',
-        label_title: 'Definition',
-        sort_by_title: 'Sort By Acronym'
+        
+        code_title: 'Acrónimo',
+        label_title: 'Définition'
         
     });
 });
