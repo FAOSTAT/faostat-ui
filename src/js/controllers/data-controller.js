b9afce581bf22053a731cd8936a1559b97095c48
simonemurzilli <simone.murzilli@fao.org>
2016-04-26 10:27:25 +0200

    updated css

---
+++ src/js/controllers/data-controller.js
@@ -0,0 +1,41 @@
+/*global define*/
+define([
+    'controllers/base/controller',
+    'views/download-view'
+], function (Controller, View) {
+
+    'use strict';
+
+    return Controller.extend({
+
+        show: function (params, section) {
+            this.view = new View({
+                region: 'main',
+                section: section,
+                code: params.code.toUpperCase()
+            });
+        },
+
+        show_bulk_downloads: function (params) {
+            this.show(params, 'bulk');
+        },
+
+        show_interactive_download: function (params) {
+            this.show(params, 'interactive');
+        },
+
+        show_metadata: function (params) {
+            this.show(params, 'metadata');
+        },
+
+        show_about: function (params) {
+            this.show(params, 'about');
+        },
+
+        show_report: function (params) {
+            this.show(params, 'report');
+        }
+
+    });
+
+});
---
+++ src/js/lib/related_documents/related_documents.js
@@ -0,0 +1,3 @@
+/**
+ * Created by vortex on 4/25/16.
+ */
