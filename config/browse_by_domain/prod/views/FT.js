065797e7903298637129a740770f345aba0271e5
simonemurzilli <simone.murzilli@fao.org>
2016-04-29 14:33:02 +0200

    added ASTI and forestry

---
+++ config/browse_by_domain/prod/views/FT.js
@@ -0,0 +1,168 @@
+/*global define*/
+define([
+    'config/browse_by_domain/Config'
+],function (C) {
+
+    'use strict';
+
+    return {
+
+        "filter": {
+
+            defaultFilter: {
+                "domain_code": ["FM"],
+                "show_lists": false
+            },
+
+            items: [
+                {
+                    "id": "reporterarea",
+                    "type": "codelist",
+                    "parameter": "List1Codes",
+                    "componentType": {
+                        "class": "col-sm-4",
+                        "type": "dropDownList",
+                        "multiple": false
+                    },
+                    "config": {
+                        "dimension_id": "reporterarea",
+                        "defaultCodes": ["2"],
+                        "filter": {}
+                    }
+                },
+                {
+                    "id": "item",
+                    "type": "codelist",
+                    "parameter": "List4Codes",
+                    "componentType": {
+                        "class": "col-sm-4",
+                        "type": "dropDownList",
+                        "multiple": false
+                    },
+                    "config": {
+                        "dimension_id": "item",
+                        "defaultCodes": ["15"],
+                        "filter": {}
+                    }
+                },
+                {
+                    "id": "element",
+                    "type": "codelist",
+                    "parameter": "List3Codes",
+                    "componentType": {
+                        "class": "col-sm-4",
+                        "type": "dropDownList",
+                        "multiple": false
+                    },
+                    "config": {
+                        "dimension_id": "element",
+                        "defaultCodes": ["2610"],
+                        "filter": {}
+                    }
+                },
+                {
+                    "id": "year",
+                    "type": "codelist",
+                    "parameter": "List5Codes",
+                    "componentType": {
+                        "class": "col-lg-2",
+                        "type": "dropDownList-timerange"
+                    },
+                    "config": {
+                        "dimension_id": "year",
+                        "defaultCodes": ['1992'],
+                        "filter": {
+                        }
+                    }
+                },
+                C.filter.aggregation
+            ]
+        },
+
+        dashboard: {
+
+            //data base filter
+            defaultFilter: {
+                domain_codes: ['FM'],
+                List5Codes: null,
+                List6Codes: null,
+                List7Codes: null,
+                decimal_places: 2,
+                decimal_separator: ".",
+                limit: -1,
+                thousand_separator: ",",
+                "null_values": null,
+                // TODO: remove it the page_size!!!
+                page_size: 0,
+                per_page: 0,
+                page_number: 0
+            },
+
+            // labels?
+            labels: {
+                // labels to dinamically substitute the title and subtitle
+                default: {}
+            },
+
+
+            //bridge configuration
+            bridge: {
+
+                type: "faostat"
+                //requestType: 'data' // data, rankings
+
+            },
+
+            metadata: {},
+
+            items: [
+                {
+                    type: 'map',
+                    class: "col-xs-12",
+
+                    // labels?
+                    labels: {
+                        // labels to dinamically substitute the title and subtitle
+                        default: {},
+
+                        // temp[late to be applied to the config.template for the custom object
+                        template: {
+                            title: {
+                                en: "{{reporterarea}} {{item}} {{element}}",
+                                fr: "{{item}}",
+                                es: "{{item}}"
+                            },
+                            subtitle: "{{aggregation}} {{year}}"
+                        }
+                    },
+
+
+                    //height:'250px',
+                    config: {
+                        template: {
+
+                        },
+                        adapter: {
+                            dimensions: {
+                                geoDimensions: {
+                                    dimension_id: 'partnerarea',
+                                    type: 'code'
+                                }
+                            }
+                        }
+
+                    },
+                    allowedFilter: ['item', 'year', 'element', 'aggregation', 'reporterarea'],
+                    deniedTemplateFilter: [],
+                    filter: {
+                        List2Codes: ["5000>"],
+                        // TODO: remove the List1Codes (in theory should be automatically detected from the domain dimensions/schema)
+                        "group_by": 'year',
+                        "order_by": 'reporterarea'
+                    }
+                }
+            ]
+        }
+        
+    };
+});
