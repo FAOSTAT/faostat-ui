e99af8c57493dfc700d2b794ff56cf2d946fb642
simonemurzilli <simone.murzilli@fao.org>
2016-05-04 18:04:39 +0200

    added analytics configuration

--- config/Analytics.js
+++ config/Analytics.js
@@ -6,99 +6,149 @@ define(function () {
     return {
 
         // GOOGLE ANALYTICS BINDINGS
-        ANALYTICS: {
 
-            // categories
+        // categories
+        download: {
+            category: 'download',
+            action: {
+                preview_table: 'preview',
+                download_table: 'download',
+                preview_pivot: 'download',
+                download_pivot: 'download',
+            },
+            label: {
+                // section should be derived from the page viewed
+                section: '', //i.e. browse_by_domain/download_interactive from ChaplinJS etc
+            } //domain code
+        },
+
+        download_bulk: {
+            category: 'download_bulk',
+            action: {
+                download: 'download'
+            },
+            label: {
+                filename: '',
+                section: ''
+            }
+        },
+
+        metadata: {
+            category: 'metadata',
+            action: {
+                download: 'download'
+            },
+            label:  {
+                section: ''
+            }
+        },
+
+        /* methodos and standards specific tracking */
+        methodology: {
+            selection: {
+                category: 'methodology',
+                action: 'methodology_selection',
+                label: '' //code
+            }
+        },
+
+        classifications: {
+
+            selection: {
+                category: 'classifications',
+                action: 'classifications_selection',
+                label: '' //code
+            },
+
+            download: {
+                category: 'classifications',
+                action: 'download',
+                label: '' //code
+            }
+
+        },
+
+        abbreviations: {
+
+            download: {
+                category: 'abbreviations',
+                action: 'download',
+                label: ''
+            }
+            
+        },
+
+        glossary: {
+
+            download: {
+                category: 'glossary',
+                action: 'download',
+                label: ''
+            }
+            
+        },
+
+        units: {
+
+            download: {
+                category: 'glossary',
+                action: 'download',
+                label: ''
+            }
+
+        },
+
+        /* search */
+        search: {
+
+            text: {
+                category: 'search',
+                action: 'text',
+                label: '' //text searched
+            },
+
             download: {
                 category: 'download',
-                action: {
-                    preview: 'preview',
-                    download: 'download',
-                },
-                label: {
-                    section: '', //i.e. browse_by_domain/download_interactive from ChaplinJS etc
-                } //domain code
+                action: 'text',
+                label: '' //text searched
             },
 
-            download_bulk: {
-                category: 'download_bulk',
-                action: {
-                    download: 'download'
-                },
-                label: {
-                    filename: '',
-                    section: ''
-                }
+            go_to_browse: {
+                category: 'search',
+                action: 'go_to_browse',
+                label: '' // url
             },
 
-            browse_data: {
-                category: 'browse_data',
-                action: {
-                    change_selection: 'change_selection'
-                },
-                label: '' // type? domain code? code changed?
-            },
+            go_to_download: {
+                category: 'search',
+                action: 'go_to_download',
+                label: '' // url
+            }
 
-            metadata: {
-                category: 'metadata',
-                action: {
-                    download: 'download_metadata'
-                },
-                label: '' // code
-            },
+        },
 
-            compare: {
+        compare: {
+
+            add_filter: {
                 category: 'compare',
-                action: {
-                    change_selection: 'change_selection',
-                    compare_data: ''
-                },
-                label: '' // type? domain code? code changed?
+                action: 'add_filter',
+                label: ''
             },
 
-            search: {
-
+            remove_filter: {
+                category: 'compare',
+                action: 'remove_filter',
+                label: ''
             },
 
-            methodologies: {
-
-            },
-
-            classifications: {
-
-            },
-
-            units: {
-
-            },
-
-            glossary: {
-
-            },
-
-            abbreb: {
-
-            },
-
-
-            BROWSE_BY_DOMAIN: {
-                category: 'DATA_BY_DOMAIN',
-                action: {
-                    show_table: 'Show Tables - TABLE'
-                }
-            },
-            DOWNLOAD_STANDARD: {
-                category: 'DOWNLOAD_STANDARD',
-                action: {
-                    show_table: 'Show Tables - TABLE'
-                }
-            },
-            COMPARE: {
-                category: 'COMPARE',
-                action: {
-                    compare_data: 'Compare the data'
+            compare_data: {
+                category: 'compare',
+                action: 'compare_domains',
+                label: {
+                    codes: []
                 }
             }
+
         }
     };
 });
