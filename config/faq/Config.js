672ba87aac2da18fd0ecca0e65d445b540893e78
simonemurzilli <simone.murzilli@fao.org>
2016-05-05 10:41:47 +0200

    adede FAQs

--- config/faq/Config.js
+++ config/faq/Config.js
@@ -10,19 +10,47 @@ define([
 
             "en": [
                 {
-                    topic: "Topic1",
+                    topic: "",
                     questions: [
                         {
-                            question: "Cos'è ArcGIS Online?",
-                            answer: " è una piattaforma online di collare e condividere mappe, scene, app, layer, analisi e dati. È possibile ac"
+                            question: "How do  I register to FAOSTAT?",
+                            answer: "Currently no registration is necessary though it may be introduced in future."
                         },
                         {
-                            question: "Cos'è ArcGIS Online?",
-                            answer: " è una piattaforma online di collare e condividere mappe, scene, app, layer, analisi e dati. È possibile ac"
+                            question: "I would like to publish FAOSTAT data for my research, how do I quote your data?",
+                            answer: "Please note that: <br> <ul><li>FAO remains the copyright holder of the material, and retains the right to reproduce, translate, publish, and disseminate the whole or any part of it  in print and electronic formats, and to grant others the right to do the same, as well as to incorporate material derived from the material in any subsequent work.</li><li>The use of the official emblem or other logos of FAO  is prohibited without express and prior written approval by FAO.</li><li> The material must not be used in any way that implies FAO’s endorsement of any companies, services or products.</li></ul> Due acknowledgement shall be made to FAO, with the source document cited and Web URL provided if applicable as follows: Source: Food and Agriculture Organization of the United Nations, [year], [authors], [title of publication], [URL]. Reproduced with permission <br><br> To acknowledge FAOSTAT please follow the Citation below. <br> FAO. FAOSTAT. NAME OF DATA COLLECTION (dataset). (Latest update: DATE). Accessed DATE. URI: http://faostat.fao.org/..."
+                        },
+                        {
+                            question: "NOT NEEDED! What is the difference between Browse data and Download data?",
+                            answer: "Using the Browse data option enables you to have an overall view of our data, such as the items by Domain, Cuntry and Rankings. The Download data option enables you to opt for the Bulk downloads,   and to choose more detailed options to personalize your query."
+                        },
+                        {
+                            question: "Who do I write to when I encounter discrepancies or problems related to FAOSTAT?",
+                            answer: "Please write to the FAOSTAT Help desk at <a href='faostat@fao.org'>faostat@fao.org</a> or call +39 06 5705 5303"
+                        },
+                        {
+                            question: "Where can I find resources and publications about FAOSTAT?",
+                            answer: "Our statistical publications can be accessed in the Statistics division website, under publications. They are available to download in PDF."
+                        },
+                        {
+                            question: "How do I get to know when you will have future updates?",
+                            answer: "Our Homepage has a link to the Release Calendar as well as a What’s new and Coming up section"
+                        },
+                        {
+                            question: "Can I purchase FAOSTAT data and how much does it cost?",
+                            answer: "FAOSTAT data is free and available to everyone."
+                        },
+                        {
+                            question: "I was looking for statistical data on at sub-national level, but there no information, why is that?",
+                            answer: "The data we receive mainly from the National Statistical offices are on a Country level."
+                        },
+                        {
+                            question: "What do “>” and “+” mean?",
+                            answer: "If you select ‘’ World” with the + sign, you obtain the aggregate total of all countries. If you use the ‘’ >’’ sign you obtain the same result but detailed country by country."
                         }
                     ]
                 },
-                {
+               /* {
                     topic: "Topic2",
                     questions: [
                         {
@@ -34,7 +62,7 @@ define([
                             answer: " è una piattaforma online di collare e condividere mappe, scene, app, layer, analisi e dati. È possibile ac"
                         }
                     ]
-                }
+                }*/
             ],
 
             "fr": [
