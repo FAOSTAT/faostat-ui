b03eea10daed569a0fe0015c5abff969850dcfde
simonemurzilli <simone.murzilli@fao.org>
2016-06-30 10:48:43 +0200

    updated homepage

--- src/js/views/home-view.js
+++ src/js/views/home-view.js
@@ -164,6 +164,8 @@ define([
                 var swiper = new Swiper('.swiper-container', {
                     pagination: '.swiper-pagination',
                     paginationClickable: true,
+                    preventLinks: false,
+                    preventLinksPropagation: false,
                     nextButton: '.swiper-button-next',
                     prevButton: '.swiper-button-prev',
                     parallax: true,
