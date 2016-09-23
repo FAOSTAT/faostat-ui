396c6cb33307854a695ecd327648e038b43cefa3
simonemurzilli <simone.murzilli@fao.org>
2016-09-23 17:36:55 +0200

    .

--- config/submodules/fx-map/config/config.js
+++ config/submodules/fx-map/config/config.js
@@ -3,8 +3,8 @@ define(function () {
     'use strict';
 
     return {
-        //WMS_URL: "http://fenixservices.fao.org/faostat/geoserver"
-        WMS_URL: "http://fenix.fao.org:15100/geoserver/faostat/wms"
+        WMS_URL: "http://fenixservices.fao.org/faostat/geoserver"
+        //WMS_URL: "http://fenix.fao.org:15100/geoserver/faostat/wms"
 
     };
 
