e07f1e26ed047f0fb51172d55d5f2b49cc5cfa93
simonemurzilli <simone.murzilli@fao.org>
2016-10-26 10:45:50 +0200

    fixed label

--- i18n/es/common.js
+++ i18n/es/common.js
@@ -23,8 +23,8 @@ define({
     filter_domains: "Filtrar dominios",
     map_disclaimer: "Las denominaciones empleadas en los mapas y la forma en que aparecen presentados los datos no implican, por parte de la FAO, juicio alguno sobre la condición jurídica de países, territorios o zonas marítimas, ni respecto de la delimitación de sus fronteras. Sudán del Sur declaró su independencia el 9 de julio de 2011. Debido a la disponibilidad de datos, la evaluación presentada en el mapa para Sudán y Sudán del Sur refleja la situación hasta el año 2011 para la antigua Sudán.",
     help_us_improve_this_site: "Ayúdenos a mejorar este sitio",
-    download_data: "Descargar datos",
-    show_data: "Mostrar datos",
+    download_data: "Descargar Datos",
+    show_data: "Mostrar Datos",
     data_preview: "Previsualización de datos",
 
     // Table
@@ -43,7 +43,7 @@ define({
     metadata: 'Metadatos',
     bulk_downloads: 'Descarga en bloque',
     interactive_download: 'Descargar datos',
-    visualize_data: "Visualizar atos",
+    visualize_data: "Visualizar Datos",
     search: 'Buscar',
     data: 'Datos',
     topics: 'Temas',
